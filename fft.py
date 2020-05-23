a191416e69ab8d17c824c8f93b9e9cdb1c0d2549
soildom <1105279164@qq.com>
2020-05-23 14:08:35 +0800

    update

--- fft.py
+++ fft.py
@@ -3,80 +3,107 @@ import shutil
 
 import cv2
 import numpy as np
+from numpy.fft import *
 import matplotlib.pyplot as plt
-from matplotlib.pylab import mpl
-from scipy.fftpack import fft, ifft
+
+
+def multi_channel_fft(img):
+    img_fft = np.zeros(img.shape, dtype=np.complex128)
+    for i in range(img.shape[2]):
+        img_fft[:, :, i] = fft2(img[:, :, i])
+    return img_fft
+
+
+def multi_channel_ifft(img_fft):
+    img = np.zeros(img_fft.shape, dtype=np.complex128)
+    for i in range(img_fft.shape[2]):
+        img[:, :, i] = ifft2(img_fft[:, :, i])
+    return img
 
 
 def fft_transfer(content_path, style_path):
+    dpi = 400
+    fontsize = 5
     save_path = 'Output/fft/'
     if os.path.exists(save_path):
         shutil.rmtree(save_path)
     os.makedirs(save_path)
 
-    mpl.rcParams['font.sans-serif'] = ['SimHei']  # 显示中文
-    mpl.rcParams['axes.unicode_minus'] = False  # 显示负号
-    content = cv2.imread(content_path)
-    style = cv2.imread(style_path)
-    style = cv2.resize(style, (content.shape[1], content.shape[0]))
-    content = content / 255
-    style = style / 255
-    y1 = np.array(content)
-    y2 = np.array(style)
-    fft_y1 = fft(y1)
-    fft_y2 = fft(y2)
-    abs_y1 = np.abs(fft_y1)  # 取复数的绝对值，即复数的模(双边频谱)
-    abs_y2 = np.abs(fft_y2)
-    angle_y1 = np.angle(fft_y1)  # 取复数的角度
-    angle_y2 = np.angle(fft_y2)
-    xfr = np.multiply(abs_y1, np.cos(angle_y2)) + np.multiply(abs_y1, np.sin(angle_y2)) * 1j  # 进行图片的幅度与频度的重新叠加
-    yfr = np.multiply(abs_y2, np.cos(angle_y1)) + np.multiply(abs_y2, np.sin(angle_y1)) * 1j
-    xr = abs(ifft(xfr))
-    yr = abs(ifft(yfr))
+    content = cv2.imread(content_path) / 255.0
+    style = cv2.resize(cv2.imread(style_path), (content.shape[1], content.shape[0])) / 255.0
 
-    plt.subplot(441)
-    plt.imshow(content)
-    plt.title('原始图像一', fontsize=9)
+    content_fft = multi_channel_fft(content)
+    style_fft = multi_channel_fft(style)
 
-    plt.subplot(444)
-    plt.imshow(style)
-    plt.title('原始图像二', fontsize=9)
+    content_amplitude = np.abs(content_fft)  # 取复数的绝对值，即复数的模(双边频谱)
+    style_amplitude = np.abs(style_fft)
 
-    plt.subplot(446)
-    plt.imshow(abs_y1)
-    plt.title('振幅谱一', fontsize=9)
+    content_phase = np.angle(content_fft)  # 取复数的角度
+    style_phase = np.angle(style_fft)
 
-    plt.subplot(447)
-    plt.imshow(abs_y2)
-    plt.title('振幅谱二', fontsize=9)
+    # 进行图片的幅度与频度的重新叠加
+    target1_fft = content_amplitude * (np.cos(style_phase) + 1j * np.sin(style_phase))
+    target2_fft = style_amplitude * (np.cos(content_phase) + 1j * np.sin(content_phase))
+    target1 = np.abs(multi_channel_ifft(target1_fft)).clip(min=0, max=1)
+    target2 = np.abs(multi_channel_ifft(target2_fft)).clip(min=0, max=1)
 
-    plt.subplot(449)
-    plt.imshow(angle_y1)
-    plt.title('相位谱一', fontsize=9)
+    plt.rcParams['savefig.dpi'] = dpi
+    plt.rcParams['figure.dpi'] = dpi
 
-    plt.subplot(4, 4, 12)
-    plt.imshow(angle_y2)
-    plt.title('相位谱二', fontsize=9)
+    plt.subplot(4, 4, 5)
+    plt.imshow(content[:, :, ::-1])
+    plt.title('Content', fontsize=fontsize, pad=0)
+    plt.axis('off')
+
+    plt.subplot(4, 4, 8)
+    plt.imshow(style[:, :, ::-1])
+    plt.title('Style', fontsize=fontsize, pad=0)
+    plt.axis('off')
+
+    plt.subplot(4, 4, 2)
+    plt.imshow(np.log(content_amplitude[:, :, 0]), 'gray')
+    plt.title('Content Amplitude B', fontsize=fontsize, pad=0)
+    plt.axis('off')
+    plt.subplot(4, 4, 6)
+    plt.imshow(np.log(content_amplitude[:, :, 1]), 'gray')
+    plt.title('Content Amplitude G', fontsize=fontsize, pad=0)
+    plt.axis('off')
+    plt.subplot(4, 4, 10)
+    plt.imshow(np.log(content_amplitude[:, :, 2]), 'gray')
+    plt.title('Content Amplitude R', fontsize=fontsize, pad=0)
+    plt.axis('off')
+
+    plt.subplot(4, 4, 3)
+    plt.imshow(np.log(style_amplitude[:, :, 0]), 'gray')
+    plt.title('Style Amplitude B', fontsize=fontsize, pad=0)
+    plt.axis('off')
+    plt.subplot(4, 4, 7)
+    plt.imshow(np.log(style_amplitude[:, :, 1]), 'gray')
+    plt.title('Style Amplitude G', fontsize=fontsize, pad=0)
+    plt.axis('off')
+    plt.subplot(4, 4, 11)
+    plt.imshow(np.log(style_amplitude[:, :, 2]), 'gray')
+    plt.title('Style Amplitude R', fontsize=fontsize, pad=0)
+    plt.axis('off')
 
     plt.subplot(4, 4, 14)
-    plt.imshow(xr)
-    plt.title('改变风格后的图像一', fontsize=9)
+    plt.imshow(target1[:, :, ::-1])
+    plt.title('Target-1', fontsize=fontsize, pad=0)
+    plt.axis('off')
 
     plt.subplot(4, 4, 15)
-    plt.imshow(yr)
-    plt.title('改变风格后的图像二', fontsize=9)
+    plt.imshow(target2[:, :, ::-1])
+    plt.title('Target-2', fontsize=fontsize, pad=0)
+    plt.axis('off')
 
+    plt.savefig(save_path + 'Plot.png', bbox_inches='tight')
     plt.show()
 
-    cv2.imwrite(save_path + 'Content.png', (content * 255).astype(np.uint8).clip(min=0, max=255))
-    cv2.imwrite(save_path + 'Style.png', (style * 255).astype(np.uint8).clip(min=0, max=255))
-    cv2.imwrite(save_path + 'Amplitude-1.png', (abs_y1 * 255).astype(np.uint8).clip(min=0, max=255))
-    cv2.imwrite(save_path + 'Amplitude-2.png', (abs_y2 * 255).astype(np.uint8).clip(min=0, max=255))
-    cv2.imwrite(save_path + 'Phase-1.png', (angle_y1 * 255).astype(np.uint8).clip(min=0, max=255))
-    cv2.imwrite(save_path + 'Phase-2.png', (angle_y2 * 255).astype(np.uint8).clip(min=0, max=255))
-    cv2.imwrite(save_path + 'Target-1.png', (xr * 255).astype(np.uint8).clip(min=0, max=255))
-    cv2.imwrite(save_path + 'Target-2.png', (yr * 255).astype(np.uint8).clip(min=0, max=255))
+    cv2.imwrite(save_path + 'Content.png', (content * 255).astype(np.uint8))
+    cv2.imwrite(save_path + 'Style.png', (style * 255).astype(np.uint8))
+    cv2.imwrite(save_path + 'Target-1.png', (target1 * 255).astype(np.uint8))
+    cv2.imwrite(save_path + 'Target-2.png', (target2 * 255).astype(np.uint8))
 
 
 if __name__ == '__main__':
-    fft_transfer('ContentImage/1.png', 'StyleImage/1.jpg')
+    fft_transfer('ContentImage/IMG_0375.jpeg', 'StyleImage/神奈川沖浪裏.jpg')
