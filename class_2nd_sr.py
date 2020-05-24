178456854ac54490c71c0e412a6a789e29b559ea
soildom <1105279164@qq.com>
2020-05-24 11:27:27 +0800

    update

--- class_2nd_sr.py
+++ class_2nd_sr.py
@@ -115,7 +115,7 @@ def train():
                     running_pixel_loss = 0.0
 
         scheduler1.step()
-        torch.save(net.state_dict(), 'Model/class_2nd/sr.pth')
+        torch.save(net.state_dict(), 'Model/class_2nd/sr-pixel.pth')
 
     for epoch in range(1, epoch_num // 2 + 1):
         running_vgg_loss = 0.0
@@ -147,7 +147,7 @@ def train():
                     running_pixel_loss = 0.0
 
         scheduler2.step()
-        torch.save(net.state_dict(), 'Model/class_2nd/sr.pth')
+        torch.save(net.state_dict(), 'Model/class_2nd/sr-vgg.pth')
 
 
 def generate(lr_path):
@@ -161,7 +161,7 @@ def generate(lr_path):
         denormalize = transforms.Normalize(mean=[-2.12, -2.04, -1.80], std=[4.37, 4.46, 4.44])
 
         net = SRNet()
-        net.load_state_dict(torch.load('Model/class_2nd/sr.pth', map_location=device))
+        net.load_state_dict(torch.load('Model/class_2nd/sr-pixel.pth', map_location=device))
         net = net.eval()
         sr = denormalize(net(transform(lr).unsqueeze(0).to(device)).squeeze(0)).clamp_(0, 1)
         torchvision.utils.save_image(torch.cat((transforms.ToTensor()(lr_bicubic), sr), 2), 'tmp.png')
